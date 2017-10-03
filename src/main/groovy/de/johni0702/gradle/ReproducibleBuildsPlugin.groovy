a524ada411315aa9b2a58eedcaaa834015e56721
Jonas Herzig <me@johni0702.de>
2017-10-03 10:50:08 +0200

    Fix timestamp reproducibility of repacked archives

--- src/main/groovy/de/johni0702/gradle/ReproducibleBuildsPlugin.groovy
+++ src/main/groovy/de/johni0702/gradle/ReproducibleBuildsPlugin.groovy
@@ -1,5 +1,7 @@
 package de.johni0702.gradle;
 
+import java.util.Calendar
+import java.util.GregorianCalendar
 import java.util.jar.JarEntry
 import java.util.jar.JarFile
 import java.util.jar.JarOutputStream
@@ -9,6 +11,10 @@ import org.gradle.api.Project;
 import org.gradle.api.tasks.bundling.AbstractArchiveTask;
 
 class ReproducibleBuildsPlugin implements Plugin<Project> {
+    // Why can we not just use 0?
+    // This is why: https://github.com/gradle/gradle/blob/e411cc70f9645138232b427ed63159d7cbc00523/subprojects/core/src/main/java/org/gradle/api/internal/file/archive/ZipCopyAction.java#L42-L56
+    private static final long CONSTANT_TIME_FOR_ZIP_ENTRIES = new GregorianCalendar(1980, Calendar.FEBRUARY, 1, 0, 0, 0).timeInMillis
+
     void apply(Project project) {
         project.extensions.create("reproducibleBuilds", ReproducibleBuildsExtension)
 
@@ -37,7 +43,7 @@ class ReproducibleBuildsPlugin implements Plugin<Project> {
             JarFile jf = new JarFile(file)
             jf.entries().unique {it.name}.sort {it.name}.each {
                 def copy = new JarEntry(it.name)
-                copy.time = 0
+                copy.time = CONSTANT_TIME_FOR_ZIP_ENTRIES
                 out.putNextEntry(copy)
                 out << jf.getInputStream(it)
             }
